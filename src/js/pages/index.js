daf53dde22d7f4081741789714664c91ac1bbaf3
Armando Sierra <armando.sierra.fimbres@gmail.com>
2018-01-20 15:56:48 -0700

    init platzi-video

---
+++ src/js/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {render} from 'react-dom';
+import Label from '../components/label';
+
+const App = document.getElementById('app')
+
+render(<Label title="Hola Mundo!" />, App)
